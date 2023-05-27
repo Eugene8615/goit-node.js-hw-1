a4de76f65290ec3ff303de0e5c1f0f2592308352
Eugene8615 <reznichenko8615@gmail.com>
2023-05-27 04:01:48 +0300

    1

---
+++ README.md
@@ -0,0 +1,4 @@
+1. https://ibb.co/2Fq73Ld
+2. https://ibb.co/g9ScDBZ
+3. https://ibb.co/b3gwrn7
+4. https://ibb.co/VgSVdLn
---
+++ index.js
@@ -0,0 +1,28 @@
+const argv = require("yargs").argv;
+
+const contacts = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const listContacts = await contacts.listContacts();
+      return console.log(listContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const addContact = await contacts.addContact(name, email, phone);
+      return console.log(addContact);
+
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
